bc0b293f62a27e4a88f18e7517c3789dfc7ce08b
Ryan Duffield <duffield@gmail.com>
2011-04-11 19:08:01 +0000

    Refactored and added a test class to provide plugin metadata

--- plugins.py
+++ plugins.py
@@ -6,13 +6,24 @@ Classes for plugin download, installation, and registration.
 """
 
 
+import os
+import platform
 from optparse import OptionParser
+
+python_version = platform.python_version_tuple()
+
+if int(python_version[1]) >= 6:
+    import json
+else:
+    import minjson
 
 class App(object):
     """
-    Entry point to the plugin helper application.
+    Class for collecting arguments and options for the plugin
+    download and installation process.
 
     """
+
     def __init__(self):
         usage = 'usage: %prog [options] key'
         self.parser = OptionParser(usage=usage)
@@ -20,10 +31,41 @@ class App(object):
                                default=True, help='run in verbose mode [default]')
 
     def run(self):
+        """
+        Entry point to the plugin helper application.
+
+        """
         (options, args) = self.parser.parse_args()
-        print options, args
         downloader = PluginDownloader(args[0])
         downloader.start()
+
+class PluginMetadata(object):
+    def __init__(self):
+        pass
+
+    def get(self):
+        raise Exception, 'Sub-classes to provide implementation.'
+
+    def json(self):
+        metadata = self.get()
+        print metadata
+        if json:
+            return json.loads(metadata)
+        else:
+            return minjson.safeRead(metadata)
+
+class FilePluginMetadata(PluginMetadata):
+    """
+    File-based metadata provider, for testing purposes.
+
+    """
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'tests/plugin.json')
+        print 'Reading plugin data from %s' % path
+        f = open(path, 'r')
+        data = f.read()
+        f.close()
+        return data
 
 class PluginDownloader(object):
     """
@@ -34,8 +76,8 @@ class PluginDownloader(object):
 	    self.key = key
 
     def start(self):
-        print 'downloading plugin...'
-        pass
+        metadata = FilePluginMetadata().json()
+        print 'Retrieved metadata.'
 
 if __name__ == '__main__':
     app = App()
