305b83c424a3867e66d517a2713fc43faa76a8c9
Ryan Duffield <duffield@gmail.com>
2011-06-27 20:11:16 +0000

    Typo

--- checks.py
+++ checks.py
@@ -2391,7 +2391,7 @@ class checks:
 		ioStats = self.getIOStats();
 		cpuStats = self.getCPUStats();
 		
-		if len(processes > 4194304):
+		if len(processes) > 4194304:
 			self.mainLogger.warn('doChecks: process list larger than 4MB limit, so it has been stripped')
 			
 			processes = []
