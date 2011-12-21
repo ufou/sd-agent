3a2093063f278c2b8cf2647aa1dc48ccae67dde5
Ryan Duffield <duffield@gmail.com>
2011-12-21 22:33:24 +0000

    [AGENT-181] Handle boolean processes value
    It is possible for `processes` to be False, when
    obtaining the list results in an error.  This needs
    to be handled properly since calling len() on a bool
    will fail.

--- checks.py
+++ checks.py
@@ -2350,7 +2350,7 @@ class checks:
 		ioStats = self.getIOStats();
 		cpuStats = self.getCPUStats();
 		
-		if len(processes) > 4194304:
+		if processes != False and len(processes) > 4194304:
 			self.mainLogger.warn('doChecks: process list larger than 4MB limit, so it has been stripped')
 			
 			processes = []
