2fac0098ad4f13ade9db9b96103cd53eb5f82f16
David Mytton <david@mytton.net>
2012-09-22 10:59:25 +0100

    Added plugin directory path logging

--- checks.py
+++ checks.py
@@ -2145,12 +2145,16 @@ class checks:
 
 		if 'pluginDirectory' in self.agentConfig and self.agentConfig['pluginDirectory'] != '':
 
+			self.mainLogger.info('getPlugins: pluginDirectory %s', self.agentConfig['pluginDirectory'])
+
 			if os.access(self.agentConfig['pluginDirectory'], os.R_OK) == False:
 				self.mainLogger.warning('getPlugins: Plugin path %s is set but not readable by agent. Skipping plugins.', self.agentConfig['pluginDirectory'])
 
 				return False
 
 		else:
+			self.mainLogger.debug('getPlugins: pluginDirectory not set')
+
 			return False
 
 		# Have we already imported the plugins?
