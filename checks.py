a55c184919107022bad64afeccb040d08b69c793
Chris Hannam <bassdread@gmail.com>
2013-10-12 22:29:24 +0100

    was ok the first time

--- checks.py
+++ checks.py
@@ -438,7 +438,7 @@ class checks:
 						values = re.findall(itemRegexp, line)
 
 				if values and headers:
-					cpuStats['CPU0'] = dict(zip(headers, values))
+					cpuStats['CPUs'] = dict(zip(headers, values))
 
 			except Exception, ex:
 				import traceback
