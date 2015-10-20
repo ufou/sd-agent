b95155c38ae72af0a4237ab105415e68891c246c
Carlos Perelló Marín <carlos@serverdensity.com>
2015-10-20 23:35:15 +0200

    Fixed logger reference

--- checks/server_density/plugins.py
+++ checks/server_density/plugins.py
@@ -36,9 +36,9 @@ class Plugins(Check):
                 config_path = os.path.join(config_path, 'plugins.cfg')
 
             if not os.access(config_path, os.R_OK):
-                self.log.error(
+                self.logger.error(
                     'Unable to read the config file at ' + config_path)
-                self.log.error(
+                self.logger.error(
                     'Using no config...')
                 return raw_config
 
@@ -54,35 +54,35 @@ class Plugins(Check):
                     raw_config[section][option] = config.get(section, option)
 
         except ConfigParser.ParsingError:
-            self.log.error(
+            self.logger.error(
                 "v1 Plugins config file not found or incorrectly formatted.")
 
         return raw_config
 
     def check(self, agentConfig):
 
-        self.log.debug('getPlugins: start')
+        self.logger.debug('getPlugins: start')
 
         plugin_directory = agentConfig.get('plugin_directory', None)
         if plugin_directory:
 
-            self.log.info(
+            self.logger.info(
                 'getPlugins: plugin_directory %s', plugin_directory)
 
             if not os.access(plugin_directory, os.R_OK):
-                self.log.warning(
+                self.logger.warning(
                     'getPlugins: Plugin path %s is set but not readable by ' +
                     'agent. Skipping plugins.', plugin_directory)
                 return False
         else:
-            self.log.debug('getPlugins: plugin_directory not set')
+            self.logger.debug('getPlugins: plugin_directory not set')
 
             return False
 
         # Have we already imported the plugins?
         # Only load the plugins once
         if self.plugins is None:
-            self.log.debug(
+            self.logger.debug(
                 'getPlugins: initial load from %s', plugin_directory)
 
             sys.path.append(plugin_directory)
@@ -93,7 +93,7 @@ class Plugins(Check):
             # Loop through all the plugin files
             for root, dirs, files in os.walk(plugin_directory):
                 for name in files:
-                    self.log.debug('getPlugins: considering: %s', name)
+                    self.logger.debug('getPlugins: considering: %s', name)
 
                     name = name.split('.', 1)
 
@@ -101,7 +101,7 @@ class Plugins(Check):
                     try:
                         if name[1] == 'py':
 
-                            self.log.debug(
+                            self.logger.debug(
                                 'getPlugins: ' + name[0] + '.' + name[1] +
                                 ' is a plugin')
 
@@ -112,13 +112,13 @@ class Plugins(Check):
             # Loop through all the found plugins, import them then create new
             # objects
             for plugin_name in plugins:
-                self.log.debug('getPlugins: loading %s', plugin_name)
+                self.logger.debug('getPlugins: loading %s', plugin_name)
 
                 plugin_path = os.path.join(
                     plugin_directory, '%s.py' % plugin_name)
 
                 if not os.access(plugin_path, os.R_OK):
-                    self.log.error(
+                    self.logger.error(
                         'getPlugins: Unable to read %s so skipping this '
                         'plugin.', plugin_path)
                     continue
@@ -130,75 +130,75 @@ class Plugins(Check):
                     import imp
                     imported_plugin = imp.load_source(plugin_name, plugin_path)
 
-                    self.log.debug('getPlugins: imported %s', plugin_name)
+                    self.logger.debug('getPlugins: imported %s', plugin_name)
 
                     # Find out the class name and then instantiate it
                     plugin_class = getattr(imported_plugin, plugin_name, None)
                     if plugin_class is None:
-                        self.log.info(
+                        self.logger.info(
                             'getPlugins: Unable to locate class %s in %s, '
                             'skipping', plugin_name, plugin_path)
                         continue
 
                     try:
                         plugin_obj = plugin_class(
-                            agentConfig, self.log, self.raw_config)
+                            agentConfig, self.logger, self.raw_config)
                     except TypeError:
 
                         try:
                             plugin_obj = plugin_class(
-                                agentConfig, self.log)
+                                agentConfig, self.logger)
                         except TypeError:
                             # Support older plugins.
                             plugin_obj = plugin_class()
 
-                    self.log.debug('getPlugins: instantiated %s', plugin_name)
+                    self.logger.debug('getPlugins: instantiated %s', plugin_name)
 
                     # Store in class var so we can execute it again on the
                     # next cycle
                     self.plugins.append(plugin_obj)
 
                 except Exception:
-                    self.log.error(
+                    self.logger.error(
                         'getPlugins (%s): exception = %s', plugin_name,
                         traceback.format_exc())
 
         # Now execute the objects previously created
         if self.plugins is not None:
-            self.log.debug('getPlugins: executing plugins')
+            self.logger.debug('getPlugins: executing plugins')
 
             # Execute the plugins
             output = {}
 
             for plugin in self.plugins:
-                self.log.info(
+                self.logger.info(
                     'getPlugins: executing  %s', plugin.__class__.__name__)
 
                 try:
                     value = plugin.run()
                     if value:
                         output[plugin.__class__.__name__] = value
-                        self.log.debug(
+                        self.logger.debug(
                             'getPlugins: %s output: %s',
                             plugin.__class__.__name__,
                             output[plugin.__class__.__name__])
-                        self.log.info(
+                        self.logger.info(
                             'getPlugins: executed %s',
                             plugin.__class__.__name__)
                     else:
-                        self.log.info(
+                        self.logger.info(
                             'getPlugins: executed %s but returned no data',
                             plugin.__class__.__name__)
                 except Exception:
-                    self.log.error(
+                    self.logger.error(
                         'getPlugins: exception = %s', traceback.format_exc())
 
-            self.log.debug('getPlugins: returning')
+            self.logger.debug('getPlugins: returning')
             # Each plugin should output a dictionary so we can convert it to
             # JSON later
             return output
 
         else:
-            self.log.debug('getPlugins: no plugins, returning false')
+            self.logger.debug('getPlugins: no plugins, returning false')
 
             return False
