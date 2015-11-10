c304a70bb1ba7d73f1ed825cc8a1979fd51eb0af
Carlos Perelló Marín <carlos@serverdensity.com>
2015-11-10 20:37:21 +0100

    Fixed broken space

--- tests/core/test_transaction.py
+++ tests/core/test_transaction.py
@@ -131,7 +131,7 @@ class TestTransaction(unittest.TestCase):
 
         transaction = MetricTransaction(None, {}, "msgtype")
         endpoints = [transaction.get_url(e) for e in transaction._endpoints]
-        # Direct metric submission is not being enabled.
+        # Direct metric submission is not being enabled.
         #expected = ['https://foo.bar.com/intake/msgtype?agent_key=foo']
         expected = []
         self.assertEqual(endpoints, expected, (endpoints, expected))
