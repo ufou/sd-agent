229d5c211f2e6432ff0a1b96a6da29d181dbe628
Carlos Perelló Marín <carlos@serverdensity.com>
2015-12-18 00:20:12 +0100

    More test fixes

--- tests/checks/mock/test_directory.py
+++ tests/checks/mock/test_directory.py
@@ -98,9 +98,9 @@ class DirectoryTestCase(AgentCheckTest):
             'instances': config_stubs
         }
 
-        self.run_check(config)
-
         for config in config_stubs:
+            self.run_check({'instances': [config]})
+
             dirtagname = config.get('dirtagname', "name")
             name = config.get('name', self.temp_dir)
             dir_tags = [dirtagname + ":%s" % name]
@@ -133,9 +133,9 @@ class DirectoryTestCase(AgentCheckTest):
             'instances': config_stubs
         }
 
-        self.run_check(config)
-
         for config in config_stubs:
+            self.run_check({'instances': [config]})
+
             dirtagname = config.get('dirtagname', "name")
             name = config.get('name', self.temp_dir)
             filetagname = config.get('filetagname', "filename")
