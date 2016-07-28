f74bd5a0a894a988a6156a9a3b172e16ba3b460f
Victor Jalencas <victor@serverdensity.com>
2016-07-28 11:41:44 +0200

    Unquote rake command

--- linux_script.sh
+++ linux_script.sh
@@ -7,5 +7,5 @@ mkdir -p $INTEGRATIONS_DIR
 ls -al $INTEGRATIONS_DIR
 rm -rf /home/travis/virtualenv/python$TRAVIS_PYTHON_VERSION.9/lib/python$TRAVIS_PYTHON_VERSION/site-packages/pip-6.0.7.dist-info
 rm -rf /home/travis/virtualenv/python$TRAVIS_PYTHON_VERSION.9/lib/python$TRAVIS_PYTHON_VERSION/site-packages/setuptools-12.0.5.dist-info
-'rake ci:run'
+rake ci:run
 ls -al $INTEGRATIONS_DIR
