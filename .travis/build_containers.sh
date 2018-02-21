5cd8ec959bd683e381058284fb81d8d9bb5f8d89
Enrique J. Hernández Blasco <quique@serverdensity.com>
2018-02-21 16:26:21 +0100

    Make build_containers work for plugins
    By passing a second parameter which is the suffix plugins

--- .travis/build_containers.sh
+++ .travis/build_containers.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 set -ev
-cd ${1:-.travis/dockerfiles}
+
+dockerfiles_dir=${1:-.travis/dockerfiles}
+suffix=$2
+
+if [ -n "${suffix}" ]; then
+    suffix=_$suffix
+fi
+
+cd "$dockerfiles_dir"
 if [ ! -d "$CACHE_DIR" ]; then
     sudo mkdir "$CACHE_DIR"
 fi
 
 for distro in *;
 do
-    echo -en "travis_fold:start:build_${distro}_container\\r"
-    CACHE_FILE_VAR="CACHE_FILE_${distro}"
-    DOCKER_CACHE=${!CACHE_FILE_VAR}
-    if [ ! -f "$DOCKER_CACHE"  ]; then
+    container_name=${distro}${suffix}
+    echo -en "travis_fold:start:build_${container_name}_container\\r"
+    cache_file_var="CACHE_FILE_${container_name}"
+    docker_cache=${!cache_file_var}
+    if [ ! -f "$docker_cache"  ]; then
         cd "$distro"
-        docker build -t serverdensity:"${distro}" .
+        docker build -t serverdensity:"${container_name}" .
         cd ..
-        docker save serverdensity:${distro} | gzip > "$DOCKER_CACHE";
+        docker save serverdensity:${container_name} | gzip > "$docker_cache"
     fi
-    echo -en "travis_fold:end:build_${distro}_container\\r"
+    echo -en "travis_fold:end:build_${container_name}_container\\r"
 done
